7ccd447686ee8a98a7bc9b56b1490ada43ed3123
AlexDaniel0295 <97924250+AlexDaniel0295@users.noreply.github.com>
2025-12-13 22:24:44 -0500

    S2/L04: bears task solved

--- src/stores/bears/bears.store.ts
+++ src/stores/bears/bears.store.ts
@@ -1,13 +1,13 @@
-import { create } from 'zustand'
+import { create } from "zustand";
 
 interface BearState {
     blackBears: number;
     polarBears: number;
     pandaBears: number;
 
-	increaseBlackBears: (by: number) => void;
-	increasePolarBears: (by: number) => void;
-	increasePandaBears: (by: number) => void;
+    increaseBlackBears: (by: number) => void;
+    increasePolarBears: (by: number) => void;
+    increasePandaBears: (by: number) => void;
 }
 
 export const useBearStore = create<BearState>((set) => ({
@@ -15,7 +15,10 @@ export const useBearStore = create<BearState>((set) => ({
     polarBears: 5,
     pandaBears: 1,
 
-	increaseBlackBears: (by: number) => set((state) => ({ blackBears: state.blackBears + by })),
-	increasePolarBears: (by: number) => set((state) => ({ polarBears: state.polarBears + by })),
-	increasePandaBears: (by: number) => set((state) => ({ pandaBears: state.pandaBears + by })),
+    increaseBlackBears: (by: number) =>
+        set((state) => ({ blackBears: state.blackBears + by })),
+    increasePolarBears: (by: number) =>
+        set((state) => ({ polarBears: state.polarBears + by })),
+    increasePandaBears: (by: number) =>
+        set((state) => ({ pandaBears: state.pandaBears + by })),
 }));
